65ccc45a187a367dd3dca47ca222073802c2315f
Ayush Singh Bora <ayusinghbora27@gmail.com>
2026-04-01 20:00:49 +0530

    first commit

--- DgC.py
+++ DgC.py
@@ -3,13 +3,26 @@ from time import strftime
 
 root = tk.Tk()
 root.title('Digital Clock')
+t = input("Enter your text")
+root.attributes("-fullscreen",True)
+#root.attributes("-topmost",True)
+root.configure(background="skyblue")
+message_label = tk.Label(root, text=t,font=("calibri",70,"bold"), background="skyblue",foreground="green")
+message_label.pack(side="top",pady=20)
 
 def time():
-    string = strftime("%H:%M:%S %p \n %D \n %A" )
-    label.config(text=string)
-    label.after(1000,time)
 
-label = tk.Label(root, font=("calibri",60, "bold"), background="blue", foreground=["Red"])
-label.pack(anchor="center")
+    string = strftime("%H:%M:%S %p \n %D \n %A" )
+    clock_label.config(text=string)
+    clock_label.after(1000,time)
+
+clock_label = tk.Label(root, font=("calibri",100, "bold"), background="skyblue", foreground=["black"])
+clock_label.pack(anchor="center")
+
+
+
+clock_label.pack(expand=True,fill="both")
+
 time()
+root.bind("<Escape>",lambda e:root.destroy())
 root.mainloop()
